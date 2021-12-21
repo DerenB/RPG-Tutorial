fde6a7c1abc42e10537a1a459a3998799b602289
Deren B <dbozerprogramming@gmail.com>
2021-12-21 14:18:48 -0500

    Updating Healing Potion & Item
    Added Item inheritance to Healing Potion. Added variable to Item.

--- Engine/HealingPotion.cs
+++ Engine/HealingPotion.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class HealingPotion
+    public class HealingPotion : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int AmountToHeal { get; set; }
     }
 }
--- Engine/Item.cs
+++ Engine/Item.cs
@@ -10,6 +10,6 @@ namespace Engine
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string NamePlural { get; set; }
     }
 }
