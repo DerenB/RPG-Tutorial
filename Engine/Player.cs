a17ddf00c626a0c6a8b852badba3f31e3a61c1ee
Deren B <dbozerprogramming@gmail.com>
2021-12-20 20:11:48 -0500

    Player Class variables

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Engine
-{
-    internal class Player
-    {
+namespace Engine {
+    public class Player {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
     }
 }
