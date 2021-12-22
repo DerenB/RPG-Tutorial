74e50268a2fa44a2992a7a9ff97cacbdc2700d3a
Deren B <dbozerprogramming@gmail.com>
2021-12-21 22:14:52 -0500

    Monster Properties
    Added loot item properties to the monster class.

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,6 +13,8 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
 
         public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
         {
@@ -21,6 +23,7 @@ namespace Engine
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
