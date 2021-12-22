9fc295fdc4d3d29a93836a89f31d35a28b1df2c3
Deren B <dbozerprogramming@gmail.com>
2021-12-21 21:34:23 -0500

    Default Constructors
    Added default constructors to Location and Quest Classes.

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -20,6 +20,8 @@ namespace SuperAdventure
 
             _player = new Player();
 
+            Location location = new Location(1, "Home", "This is your house.");
+
             _player.CurrentHitPoints = 10;
             _player.MaximumHitPoints = 10;
             _player.Gold = 20;
