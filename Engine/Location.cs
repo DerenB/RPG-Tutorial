d1770531fca3a87a8c58b64d5867014d25025791
Deren B <dbozerprogramming@gmail.com>
2021-12-21 22:04:41 -0500

    Location Properties
    Added properties to Location Class

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -11,6 +11,13 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
 
         public Location(int id, string name, string description)
         {
