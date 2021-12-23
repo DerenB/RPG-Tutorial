cb752004148791a4fe037ad56fa134a99b9fd98b
Deren B <dbozerprogramming@gmail.com>
2021-12-22 21:09:05 -0500

    Final Project Build
    Builds the solution and creates the exe

--- SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure.Designer.cs
@@ -158,6 +158,7 @@
             this.btnUseWeapon.TabIndex = 11;
             this.btnUseWeapon.Text = "Use";
             this.btnUseWeapon.UseVisualStyleBackColor = true;
+            this.btnUseWeapon.Click += new System.EventHandler(this.btnUseWeapon_Click);
             // 
             // btnNorth
             // 
@@ -177,6 +178,7 @@
             this.btnUsePotion.TabIndex = 14;
             this.btnUsePotion.Text = "Use";
             this.btnUsePotion.UseVisualStyleBackColor = true;
+            this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click);
             // 
             // btnEast
             // 
